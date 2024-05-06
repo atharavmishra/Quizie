20bbf3c2de7c65aaf1db190a1e468f0d9c31d497
Atharv Mishra <atharv.mishra@unnatiagri.com>
2024-05-06 17:24:49 +0530

    worked on DashboardActivity and LoginActivity ui

--- unnati_app-AtharvApril8CreditNote/app/src/main/java/com/aksha/unnatishop/Retrofitclass/Modellist/payment_model/PaymentResponse.java
+++ unnati_app-AtharvApril8CreditNote/app/src/main/java/com/aksha/unnatishop/Retrofitclass/Modellist/payment_model/PaymentResponse.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class PaymentResponse {
     public ArrayList<Invoicepayment> getInvoicepayment() {
         return invoicepayment;
+
     }
 
     public void setInvoicepayment(ArrayList<Invoicepayment> invoicepayment) {
